4038326db4059b663c61467029feb4f44d448a97
NewMKE <39660747+NewMKE@users.noreply.github.com>
2018-05-26 19:37:43 -0500

    Create 202.py

---
+++ LeetCode Solutions/202. Happy Number/202.py
@@ -0,0 +1,19 @@
+class Solution:
+    def square_sum(self, num):
+        s = 0
+        while num > 0:
+            a = num % 10
+            num = num // 10
+            s = s + a ** 2
+        return s
+
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        st = set()
+        while not (n == 1 or n in st):
+            st.add(n)
+            n = self.square_sum(n)
+        return n == 1
