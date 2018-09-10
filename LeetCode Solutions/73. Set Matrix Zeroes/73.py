a9262262b5d7691b417dee3587fad87476f7ef80
NewMKE <39660747+NewMKE@users.noreply.github.com>
2018-09-10 13:37:01 -0500

    Create 73.py

---
+++ LeetCode Solutions/73. Set Matrix Zeroes/73.py
@@ -0,0 +1,24 @@
+class Solution:
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        rows=set()
+        columns=set()
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==0:
+                    rows.add(i)
+                    columns.add(j)
+
+        for row in rows:
+            for i in range(len(matrix[0])):
+                matrix[row][i]=0
+
+        for col in columns:
+            for i in range(len(matrix)):
+                matrix[i][col]=0
+
+        return      
